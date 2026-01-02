7399b34b825a56823621c364a0d787a11aae1af8
em-g73 <emerymgrace@gmail.com>
2026-01-02 17:15:17 -0500

    Uploaded files for part 1 (24-37)

---
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Same {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the first string:");
+        String stringOne = scan.nextLine();
+        System.out.println("Enter the second string:");
+        String stringTwo = scan.nextLine();
+        
+        if (stringOne.equals(stringTwo)) {
+            System.out.println("Same");
+        } else { 
+            System.out.println("Different");
+        }
+    }
+}
---
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class GiftTax {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Value of the gift?");
+        double giftValue = Integer.valueOf(scan.nextLine());
+        double tax = 0;
+        
+        if (giftValue >= 5000 && giftValue < 25000) {
+            tax = (100 + (giftValue-5000) * 0.08);
+        } else if (giftValue >= 25000 && giftValue < 55000) {
+            tax = (1700 + (giftValue-25000) * 0.1);
+        } else if (giftValue >= 55000 && giftValue < 200000) {
+            tax = (4700 + (giftValue-55000) * 0.12);
+        } else if (giftValue >= 200000 && giftValue < 1000000) {
+            tax = (22100 + (giftValue-200000) * 0.15);
+        } else if (giftValue >= 1000000) {
+            tax = (142100 + (giftValue-1000000) * 0.17);
+        } else {
+            System.out.println("No tax!");
+        }
+        
+        if (tax > 0) {
+            System.out.println("Tax: " + tax);
+        }
+    }
+}
